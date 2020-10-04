120d177c6c32461d154373721a2ff343095a3b79
Chrissy LeMaire <clemaire@gmail.com>
2020-10-04 12:54:41 +0200

    initial commit

---
+++ tests/public/Integration.Tests.ps1
@@ -0,0 +1,43 @@
+﻿Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\..\constants.ps1"
+
+
+Describe "Integration Tests" -Tag "IntegrationTests" {
+    BeforeEach {
+        Write-Output -Message "Next test"
+        Wait-TenServerReady -ComputerName localhost
+    }
+    Context "Connect-TenServer" {
+        It "Connects to a site" {
+            $cred = New-Object -TypeName PSCredential -ArgumentList "admin", (ConvertTo-SecureString -String admin123 -AsPlainText -Force)
+            $splat = @{
+                ComputerName         = "localhost"
+                AcceptSelfSignedCert = $true
+                Credential           = $cred
+                EnableException      = $true
+                Port                 = 8834
+            }
+            (Connect-TenServer @splat).ComputerName | Should -Be "localhost"
+        }
+    }
+
+    Context "Get-TenUser" {
+        It "Returns a user" {
+            Get-TenUser | Select-Object -ExpandProperty name | Should -Contain "admin"
+        }
+    }
+    Context "Get-TenFolder" {
+        It "Returns a folder" {
+            Get-TenFolder | Select-Object -ExpandProperty name | Should -Contain "Trash"
+        }
+    }
+
+    Context "Get-TenPlugin" {
+        It "Returns proper plugin information" {
+            $results = Get-TenPlugin -PluginId 100000
+            $results | Select-Object -ExpandProperty Name | Should -Be 'Test Plugin for esxcustomizer'
+            $results | Select-Object -ExpandProperty PluginId | Should -Be 100000
+            ($results | Select-Object -ExpandProperty Attributes).fname | Should -Be 'esxcustomizer_test.nasl'
+        }
+    }
+}
